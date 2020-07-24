77b3edfa72e67e9f8113a74ca469a42a19884fff
Vikram Singh <vikramsingh@Vikram.local>
2020-07-24 19:44:26 +0200

    Adding first blog post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "My first post",
-        "excerpt":"my first post looks just fine  ","categories": [],
+        "title": "Setting up the blog",
+        "excerpt":"Hello world!! I thought I will start of by posting on how I setup this blog. First I decided to use GitHub Pages since I already had a Github account and it sounded simple to set it up. Not simple at all as it turned out. While there are many...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/post1/",
+        "url": "http://localhost:4000/blog_setup/",
         "teaser": null
       }]
