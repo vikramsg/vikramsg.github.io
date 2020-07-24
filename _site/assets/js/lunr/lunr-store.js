6376311eb86b954183042e0c9dbfd84c33c9340f
Vikram Singh <vikramsingh@Vikram.local>
2020-07-24 17:45:50 +0200

    Testing adding _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "My first post",
+        "excerpt":"my first post looks just fine  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/post1/",
+        "teaser": null
+      }]
